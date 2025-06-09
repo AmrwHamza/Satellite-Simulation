754d0004a057be90202301265226c1e0909597d7
AmrwHamza <amrwaleedhamza@gmail.com>
2025-06-09 22:38:11 +0300

    finish eular

--- src/physics/integrators/EularComer.js
+++ src/physics/integrators/EularComer.js
@@ -3,4 +3,20 @@
 
 
 
- export function integrateEuler(){}
+ export function integrateEuler(currentPosition, currentVelocity, deltaTime){
+
+    const acceleration = calculationAcceleration(currentPosition);
+
+
+    const newVx=currentVelocity.x+acceleration.x*deltaTime;
+    const newVy=currentVelocity.y+acceleration.y*deltaTime;
+    const newVz=currentVelocity.z+acceleration.z*deltaTime;
+
+    const newPosX=currentPosition.x+newVx*deltaTime;
+    const newPosY=currentPosition.y+newVy*deltaTime;
+    const newPosZ=currentPosition.z+newVz*deltaTime;
+
+    const newPosition=new THREE.Vector3(newPosX,newPosY,newPosZ);
+    const newVelocity=new THREE.Vector3(newVx,newVy,newVz);
+    return {position:newPosition,velocity:newVelocity};
+ }
