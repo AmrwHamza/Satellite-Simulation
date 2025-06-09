c620f5d76e4ea5b04519ae8d6bd3698ac65d9dd4
AmrwHamza <amrwaleedhamza@gmail.com>
2025-06-09 20:54:05 +0300

    add scene renderer camera to main and build animate

--- src/main.js
+++ src/main.js
@@ -1,20 +1,37 @@
-import *as THREE from "three";
+import * as THREE from "three";
 import { SceneManager } from "./world/scene";
 import { CameraManager } from "./world/camera";
-import { RendererManager } from "./world/renderer"; 
+import { RendererManager } from "./world/renderer";
 
-
-let sceneManager ; 
-let cameraManager; 
-let rendererManager ;
-
-
+let sceneManager;
+let cameraManager;
+let rendererManager;
 
 function init() {
-// اول شي 
-const width = window.innerWidth;
-const height = window.innerHeight;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-  sceneManager = new SceneManager(); 
+  sceneManager = new SceneManager();
+  cameraManager = new CameraManager(width, height);
+  rendererManager = new RendererManager(width, height);
 
+  window.addEventListener("resize", onWindowResize);
+
+  animate();
 }
+
+function onWindowResize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  cameraManager.updateAspectRatio(width, height); // تحديث الكاميرا
+  rendererManager.updateSize(width, height); // تحديث العارض
+}
+
+function animate() {
+  requestAnimationFrame(animate); // تطلب من المتصفح استدعاء animate في الإطار التالي المتاح
+
+  rendererManager.render(sceneManager.getScene(), cameraManager.getCamera());
+}
+
+document.addEventListener("DOMContentLoaded", init);
